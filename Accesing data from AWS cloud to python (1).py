2b31fa2c78ec42c7231cfb96e40fe4dcd74e69a7
Splashy <94764290+SplashyZues5197@users.noreply.github.com>
2023-09-01 15:42:26 -0500

    Update Accesing data from AWS cloud to python (1).py

--- Accesing data from AWS cloud to python (1).py
+++ Accesing data from AWS cloud to python (1).py
@@ -1,213 +1,75 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
-
-
-# Dynamo
 import pandas as pd
-
-import json 
-from pprint import pprint
+import json
 from urllib.request import urlopen
-
-url = 'https://api.chess.com/pub/player/gmhikaruontwitch/games/2020/07'
-json_url = urlopen(url)
-text = json.loads(json_url.read())
-
-dynamo_list = []
-i = 0
-while i < len(text['games']):
-    
-    for items in text:
-        
-        main_dic = {}
-        main_dic['Pgn'] = text['games'][i]['pgn']
-        dynamo_list.append(main_dic)
-        i+=1
-
-
-
-
-
-# In[4]:
-
-
-s4 = []
-for i in range(0,len(dynamo_list)):
-    s4.append(dynamo_list[i]["Pgn"])
-
-
-# In[5]:
-
-
-s4
-
-
-# In[6]:
-
-
-## data schema 
-player_side = []
-result = []
-first_move = []
-legal_moves = ['1. e4','1. d4','1. c4','1. Nf3','1. Nc3','1. e3','1. d3','1. a3','1. b3','1. g3']
-won_statements= ['GMHikaruOnTwitch won by checkmate','GMHikaruOnTwitch won by resignation','GMHikaruOnTwitch won on time']
-side_statements = ['White "GMHikaruOnTwitch"','Black "GMHikaruOnTwitch"']
-for i in s4:
-    for a in legal_moves:
-        if(i.find(a)>0):
-            first_move.append(a[3:])
-    for b in won_statements:
-        if(i.find(b)>0):
-            result.append("won")
-        else:
-            result.append("lost")
-    for c in side_statements:
-        if(i.find(c)>0):
-            player_side.append(c[:5])
-            
-  
-    
-
-
-# In[7]:
-
-
-legal_moves = ['1. e4','1. d4','1. c4','1. Nf3','1. Nc3','1. e3','1. d3','1. a3','1. b3','1. g3']
-moves_played = []
-for i in dynamo_list:
-    for a in legal_moves:
-        if(i.find(a)>0):
-            first_move.append(a[3:])
-
-
-# In[8]:
-
-
-len(s4)
-
-
-# In[9]:
-
-
 import boto3
-dynamodb = boto3.resource('dynamodb')
 
-table = dynamodb.Table('chessgames')
-for i in range(1,len(s4)):
-    table.put_item(Item= {"GameNum" :i,
-                           "First_Move":first_move[i],
-                          "Players":player_side[i],
-                          "Result":result[i]
-                         } )
+# Function to fetch and process data from chess.com API
+def fetch_chess_data():
+    url = 'https://api.chess.com/pub/player/gmhikaruontwitch/games/2020/07'
+    try:
+        with urlopen(url) as json_url:
+            data = json.loads(json_url.read())
+            return data.get('games', [])
+    except Exception as e:
+        print(f"Error fetching chess data: {e}")
+        return []
 
+# Function to process chess data and store it in DynamoDB
+def process_and_store_data(data):
+    dynamo_list = []
+    legal_moves = ['1. e4', '1. d4', '1. c4', '1. Nf3', '1. Nc3', '1. e3', '1. d3', '1. a3', '1. b3', '1. g3']
 
-# In[15]:
+    for game in data:
+        pgn = game.get('pgn', '')
+        
+        first_move = None
+        for move in legal_moves:
+            if move in pgn:
+                first_move = move[3:]
+                break
+        
+        result = "lost"
+        for win_statement in ['checkmate', 'resignation', 'time']:
+            if win_statement in pgn:
+                result = "won"
+                break
+        
+        side = "White" if "White \"GMHikaruOnTwitch\"" in pgn else "Black"
+        
+        dynamo_list.append({
+            "First_Move": first_move,
+            "Players": side,
+            "Result": result
+        })
 
+    return dynamo_list
 
+# Function to store data in DynamoDB
+def store_data_in_dynamodb(data):
+    try:
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table('chessgames')
+        
+        for i, item in enumerate(data):
+            table.put_item(Item={
+                "GameNum": i + 1,
+                "First_Move": item["First_Move"],
+                "Players": item["Players"],
+                "Result": item["Result"]
+            })
+        
+        print("Data successfully stored in DynamoDB.")
+    except Exception as e:
+        print(f"Error storing data in DynamoDB: {e}")
 
-
-
-response = table.query(
-    KeyConditionExpression=Key('Result').eq('won')
-)
-
-
-# In[ ]:
-
-
-__TableName__  =  "chessgame"
-
-Primary_Column_Name = 'game_num'
-Primary_Key = 1
-columns = ['pgn']
-
-
-# In[ ]:
-
-
-print(len(dynamo_list))
-
-
-# In[ ]:
-
-
-response = table.scan()
-
-s1 = list(response["Items"])
-
-
-# In[ ]:
-
-
-pprint(s1)
-
-
-# In[ ]:
-
-
-pprint(s1[0]['game']["Pgn"])
-
-
-# In[ ]:
-
-
-s2 = []
-for i in range(1,48):
-    if s1[i]['game']["Pgn"].find('White "GMHikaruOnTwitch"') > 0:
-        s2.append(s1[i]['game']['Pgn'])
-won_statements= ['GMHikaruOnTwitch won by checkmate','GMHikaruOnTwitch won by resignation','GMHikaruOnTwitch won on time']      
-game_won = []
-for i in range(0,len(s2)):
-    for a in won_statements:
-        if(s2[i].find(a)>0):
-            game_won.append(s2[i])
-
-
-# In[ ]:
-
-
-len(game_won)
-
-
-# In[ ]:
-
-
-legal_moves = ['1. e4','1. d4','1. c4','1. Nf3','1. Nc3','1. e3','1. d3','1. a3','1. b3','1. g3']
-moves_played = []
-for i in game_won:
-    for a in legal_moves:
-        if(i.find(a)>0):
-            moves_played.append(a[3:])
-
-
-# In[ ]:
-
-
-
-unique = set(moves_played)
-unique
-
-
-# In[ ]:
-
-
-moves_played.count('g3')
-
-
-# In[1]:
-
-
-from matplotlib import pyplot as plt
-
-data3 = {'Nc3':6,'Nf3':8,'a3':1,'b3':3,'c4':2,'d4':17,'e3':1,'e4':18,'g3':2}
-move2 = list(data3.keys())
-num_games = list(data3.values())
-fig = plt.figure(figsize = (4,10))
-plt.bar(move2,num_games, color ='maroon', width = 0.4)
-plt.xlabel('Moves')
-plt.ylabel('# of games')
-plt.title('Hikaru first moves')
-plt.show()
-
+if __name__ == "__main__":
+    chess_data = fetch_chess_data()
+    if chess_data:
+        processed_data = process_and_store_data(chess_data)
+        if processed_data:
+            store_data_in_dynamodb(processed_data)
+    else:
+        print("No chess data found.")
